47a92b26f4c166308cd7153fcc1d089717125f33
Mmegwa <wisdommmegwa@gmail.com>
2022-09-09 16:59:17 -0700

    new

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,8 @@ int main(void)
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
-	if (a == 'q' | a == 'e')
-			continue;
+	if (a == 'q' || a == 'e')
+		continue;
 	putchar(a);
 	putchar('\n');
 	return (0);
