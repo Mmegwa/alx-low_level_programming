5fd26d11f4f81d14dfc42347311e5009aad573c7
Mmegwa <wisdommmegwa@gmail.com>
2022-09-09 15:29:14 -0700

    print

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,12 +8,8 @@
 int main(void)
 {
 	char a;
-
-	for (a = 'a'; a <= 'z'; a++)
-		if (a == 'q' || a == 'e')
-			continue;
-	putchar(a);
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
 	putchar('\n');
-
 	return (0);
 }
