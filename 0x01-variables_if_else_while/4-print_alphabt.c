2f6cd2bac9cf6d1017770c0464c16e128a4f17f4
Mmegwa <wisdommmegwa@gmail.com>
2022-09-09 21:47:12 -0700

    end

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,10 @@ int main(void)
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
-	if (a == 'q' || a == 'e')
-		continue;
-	putchar(a);
+	{
+		if (a != 'e' && a != 'q')
+			putchar(a);
+	}
 	putchar('\n');
 	return (0);
 }
