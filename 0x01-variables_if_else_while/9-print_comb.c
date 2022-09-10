aaa8d96e8326e4b983d18cfc3eb6dee1a75502fd
Mmegwa <wisdommmegwa@gmail.com>
2022-09-09 18:44:53 -0700

    new

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++) 
-		{putchar(num + '0');
+	for (num = 0; num <= 9; num++) {
+	putchar(num + '0');
 		if (num == 9)
 			continue;
 		putchar(',');
