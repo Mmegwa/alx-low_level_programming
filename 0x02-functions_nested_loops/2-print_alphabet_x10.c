0cab28eac10ba4a2fcf96fa16c5904ef22ff54cd
Mmegwa <wisdommmegwa@gmail.com>
2022-09-13 15:12:14 -0700

    10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - prints alphabet 10 times
  *
  * Return: Always 0;
  *
  */
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	char i;
 	char h;
