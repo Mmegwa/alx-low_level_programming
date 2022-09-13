11e65ff7cb6239fad52e1e434386e784a70d0a72
Mmegwa <wisdommmegwa@gmail.com>
2022-09-13 14:13:25 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * print_alphabet - function to print alphabet
+ *
+ */
+void print_alphabet(void);
+#endif
