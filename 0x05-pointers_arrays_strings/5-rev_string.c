5ab3331b9e0faab0266024c1ff18d986b3ede36a
Mmegwa <wisdommmegwa@gmail.com>
2022-09-19 08:54:00 -0700

    revstring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,7 +15,7 @@ void rev_string(char *s)
 	for (i = 0; i < _strlen(s) / 2; i++)
 	{
 		t = s[i];
-		s[i] = s[_strlen(s) - (i - 1)];
-		s[_strlen(s) - (i - 1)] = t;
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = t;
 	}
 }
