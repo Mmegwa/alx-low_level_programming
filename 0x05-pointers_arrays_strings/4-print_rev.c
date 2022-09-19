76204aa76cc60c7d00897154b1c1f00987568712
Mmegwa <wisdommmegwa@gmail.com>
2022-09-19 07:03:58 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,11 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = -1; i >= 0; i--)
-	{
-		_putchar(*(s + i));
-	}
 	for (i = 0; s[i] != '\0'; i++)
 	{}
+	for (i = -1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
