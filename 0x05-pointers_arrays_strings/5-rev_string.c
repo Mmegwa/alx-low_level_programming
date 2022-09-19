b950c773a152ca56086d0f1a7c8d9f34add92e89
Mmegwa <wisdommmegwa@gmail.com>
2022-09-19 08:31:20 -0700

    revstring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "2-strlen.c"
+#include <stdio.h>
 /**
  * rev_string - reverse a string value
  *
@@ -16,6 +17,5 @@ void rev_string(char *s)
 		t = s[i];
 		s[i] = s[_strlen(s) - (i - 1)];
 		s[_strlen(s) - (i - 1)] = t;
-		t++;
 	}
 }
