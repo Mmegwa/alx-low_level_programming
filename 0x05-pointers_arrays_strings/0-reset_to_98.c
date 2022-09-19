61b1792379ce9fda15a3f188bc9b0549911a976b
Mmegwa <wisdommmegwa@gmail.com>
2022-09-19 02:44:12 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset value to 98
+ *
+ * @n: pointer integer
+ *
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+this is pointers arrays and strings
