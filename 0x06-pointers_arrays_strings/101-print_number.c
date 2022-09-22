512bfdd6b7bc9e9f852facbfb2ad899abd347457
Mmegwa <wisdommmegwa@gmail.com>
2022-09-22 04:36:29 -0700

    new

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,20 @@
  */
 void print_number(int n)
 {
-	int a0, a1, a2, a3, a4, a5, a6, a7, a8, a9;
-	int s0, s1, s2, s3, s4, s5, s6, s7, s8;
+	int a0, a1, a2, a3, a4;
+	int s0, s1, s2, s3;
 
-	a0 = n / 1000000000; s0 = a0; a1 = (n / 100000000) % 10; s1 = s0 + a1;
-	a2 = (n / 10000000) % 10; s2 = s1 + a2; a3 = (n / 1000000) % 10;
-	s3 = s2 + a3; a4 = (n / 100000) % 10; s4 = s3 + a4;
-	a5 = (n / 10000) % 10; s5 = s4 + a5; a6 = (n / 1000) % 10;
-	s6 = s5 + a6; a7 = (n / 100) % 10; s7 = s6 + a7;
-	a8 = (n / 10) % 10; s8 = s7 + a8; a9 = n % 10;
+	a0 = n / 10000; s0 = a0; a1 = (n / 1000) % 10;
+	s1 = s0 + a1; a2 = (n / 100) % 10; s2 = s1 + a2;
+	a3 = (n / 10) % 10; s3 = s2 + a3; a4 = n % 10;
 	if (n < 0)
 	{
 		_putchar('-');
+		a0 *= -1;
+		a1 *= -1;
+		a2 *= -1;
+		a3 *= -1;
+		a4 *= -1;
 	}
 	if (s0 != 0)
 		_putchar('0' + a0);
@@ -29,15 +31,5 @@ void print_number(int n)
 		_putchar('0' + a2);
 	if (s3 != 0)
 		_putchar('0' + a3);
-	if (s4 != 0)
-		_putchar('0' + a4);
-	if (s5 != 0)
-		_putchar('0' + a5);
-	if (s6 != 0)
-		_putchar('0' + a6);
-	if (s7 != 0)
-		_putchar('0' + a7);
-	if (s8 != 0)
-		_putchar('0' + a8);
-	_putchar('0' + a9);
+	_putchar('0' + a4);
 }
